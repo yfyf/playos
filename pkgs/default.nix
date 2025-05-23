bfcb63236dde33414bb6198b4add4876b67fd978
Ignas Vyšniauskas <i.vysniauskas@gmail.com>
2025-05-23 11:31:01 +0300

    Add necessary libppp patch for connman to build

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -31,6 +31,9 @@ let
           url = "mirror://kernel/linux/network/connman/connman-1.42.tar.xz";
           hash = "sha256-o+a65G/Age8una48qk92Sd6JLD3mIsICg6wMqBQjwqo=";
         };
+        patches = [
+          ./create-libppp-compat.h.patch
+        ];
       });
 
     };
