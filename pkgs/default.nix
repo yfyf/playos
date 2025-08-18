faff50632dc8a1be8b2757cfaae6a0cfc08da87a
Ignas Vyšniauskas <ignas@dividat.ch>
2025-08-18 16:45:15 +0300

    Move qtvirtualkeyboard override into separate file

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -32,26 +32,8 @@ let
       });
 
       qt6 = super.qt6.overrideScope (qtself: qtsuper: {
-          qtvirtualkeyboard = qtsuper.qtvirtualkeyboard.overrideAttrs (previous: {
-                patches = [
-                    ./qtvirtualkeyboard/0001-handle-mismatch-between-latest-received-focusObject.patch
-                ];
-
-                # Note: according to Qt you should use `qt-configure-module`
-                # from qtbase (https://wiki.qt.io/Qt_Build_System_Glossary#Per-repository_Build)
-                # for configuration, but the `configure` script crashes due to
-                # unknwon type `enableLang` specified in `vkb-enable` (if used) and
-                # install fails due to incompatibilities with the nix setup.
-                #
-                # So instead we set the cmake flag directly and keep the bloated
-                # version of qtvirtualkeyboard (handwriting support, spell
-                # checker, all languages included, etc).
-                cmakeFlags = [
-                    "-DFEATURE_vkb_arrow_keynavigation=ON"
-                ] ++ (super.lib.lists.optionals (previous ? cmakeFlags) previous.cmakeFlags);
-          });
+        qtvirtualkeyboard = (import ./qtvirtualkeyboard) { pkgs = super; qt6 = qtsuper; };
       });
-
     };
 in
 
