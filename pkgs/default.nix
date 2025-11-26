304d5891a57bc16b61279e534f0d57097af33156
Ignas Vyšniauskas <ignas@dividat.ch>
2025-11-26 13:47:46 +0200

    Change nixpkgs ref to cause a massive rebuild

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,8 +4,8 @@ let
 
   nixpkgs = builtins.fetchTarball {
     # release-24.11 2025-02-10
-    url = "https://github.com/NixOS/nixpkgs/archive/edd84e9bffdf1c0ceba05c0d868356f28a1eb7de.tar.gz";
-    sha256 = "1gb61gahkq74hqiw8kbr9j0qwf2wlwnsvhb7z68zhm8wa27grqr0";
+    url = "https://github.com/NixOS/nixpkgs/archive/5ab036a8d97cb9476fbe81b09076e6e91d15e1b6.tar.gz";
+    sha256 = "0fyhfcii8flpjq644k679nb92vvdv5jwmndimyd9a999p6hzxmwh";
   };
 
   overlay =
