788841d62f02100828b60ca488b7934f72928011
Ignas Vyšniauskas <ignas@dividat.ch>
2026-08-24 17:19:19 +0300

    Add method for determining interface kind

--- controller/server/network.ml
+++ controller/server/network.ml
@@ -47,12 +47,21 @@ let init ~connman =
       in
       Lwt_result.fail exn
 
+(** A partial classification of interfaces *)
+type interface_kind =
+  | Loopback
+  | Ethernet
+  | Wireless
+  | Other
+[@@deriving sexp]
+
 module Interface = struct
   type t =
     { index : int
     ; name : string
     ; address : string
     ; link_type : string
+    ; link_status : string
     }
   [@@deriving sexp, protocol ~driver:(module Jsonm)]
 
@@ -63,6 +72,7 @@ module Interface = struct
         ; ("name", i.name |> string)
         ; ("address", i.address |> string)
         ; ("link_type", i.link_type |> string)
+        ; ("link_status", i.link_status |> string)
         ]
       |> value
     )
@@ -75,6 +85,7 @@ module Interface = struct
         ; name = dict |> List.assoc "ifname" |> Ezjsonm.get_string
         ; address = dict |> List.assoc "address" |> Ezjsonm.get_string
         ; link_type = dict |> List.assoc "link_type" |> Ezjsonm.get_string
+        ; link_status = dict |> List.assoc "operstate" |> Ezjsonm.get_string
         }
     with _ -> None
 
@@ -94,4 +105,14 @@ module Interface = struct
     |> Ezjsonm.get_list of_json
     |> List.filter_map (fun x -> x)
     |> return
+
+  let prefix_to_kind = [ ("eth", Ethernet); ("en", Ethernet); ("wl", Wireless) ]
+
+  let kind t =
+    if t.link_type = "loopback" then Loopback
+    else
+      prefix_to_kind
+      |> List.find_opt (fun (prefix, _) -> String.starts_with ~prefix t.name)
+      |> Option.map snd
+      |> Option.value ~default:Other
 end
--- controller/server/network.mli
+++ controller/server/network.mli
@@ -3,6 +3,14 @@ open Protocol_conv_jsonm
 (** Initialize Network connectivity *)
 val init : connman:Connman.Manager.t -> (unit, exn) Lwt_result.t
 
+(** A partial classification of interfaces *)
+type interface_kind =
+  | Loopback
+  | Ethernet
+  | Wireless
+  | Other
+[@@deriving sexp]
+
 module Interface : sig
   (** Network interface *)
   type t =
@@ -10,10 +18,13 @@ module Interface : sig
     ; name : string
     ; address : string
     ; link_type : string
+    ; link_status : string
     }
   [@@deriving sexp, protocol ~driver:(module Jsonm)]
 
   val to_json : t -> Ezjsonm.value
 
   val get_all : unit -> t list Lwt.t
+
+  val kind : t -> interface_kind
 end
